1d8a9cf11f1eefe90999e5f616601428fdab1f55
adshreedhar <hatrickacharya@gmail.com>
2018-01-16 22:41:33 -0500

    update exercise03

--- assignment_01/exercise_03.js
+++ assignment_01/exercise_03.js
@@ -1,8 +1,8 @@
-function isWeekend(){
-	const todayDate = new Date();
-    const day = todayDate.getDay();
-   
-    return (day === 0 || day === 6) ? 'weekend' : 'weekday';
-} 
 
-console.log(isWeekend());
+function IsWeekend(){
+    const todayDate = new Date();
+    const day = todayDate.getDay();
+    let days = ['weekend', 'weekday', 'weekday', 'weekday', 'weekday', 'weekday', 'weekend']
+    return days[day];
+} 
+console.log(IsWeekend());
